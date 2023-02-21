1c4e532db7aad3e75bccc016351ff94d01f28d0d
ogata-k <ogtkzk712@gmail.com>
2023-02-21 21:53:06 +0900

    initial commit with dependencies settings

---
+++ app/src/main/java/com/example/notetutorial/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.notetutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/// cf: https://www.youtube.com/watch?v=8_u4EOYShgo&t=5019s
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
